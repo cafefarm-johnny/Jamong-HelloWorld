0365d83b81533da1626bdd57943d97738c064219
Johnny <npcdja@gmail.com>
2020-02-16 00:50:06 +0900

    Initial Commit

---
+++ Jamong-HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jamong-HelloWorld
+//
+//  Created by Johnny Uhm on 2020/02/13.
+//  Copyright © 2020 Jamong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+    }
+}
+
